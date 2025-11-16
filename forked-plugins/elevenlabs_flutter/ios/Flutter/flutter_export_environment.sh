fd0ab35074f7cb7894a2ff4a0310504b6441a645
realkalash <1realkalash@gmail.com>
2025-11-15 22:21:03 -0500

    Increment linux build

--- forked-plugins/elevenlabs_flutter/ios/Flutter/flutter_export_environment.sh
+++ forked-plugins/elevenlabs_flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\alex\repos\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\alex\repos\fluent_gpt_app\forked-plugins\elevenlabs_flutter"
+export "FLUTTER_ROOT=/home/alex/flutter_sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/alex/repos/fluent_gpt_app/forked-plugins/elevenlabs_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=0.0.1"
